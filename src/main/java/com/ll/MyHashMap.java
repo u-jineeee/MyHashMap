8fd1aa475f56a68487978baf77f25b8de40327c6
ujineeee <yujin11006@naver.com>
2023-04-20 14:18:45 +0900

    feat : size, put

--- src/main/java/com/ll/MyHashMap.java
+++ src/main/java/com/ll/MyHashMap.java
@@ -1,5 +1,40 @@
 package com.ll;
 
 public class MyHashMap<K, V> {
+    private Object[] keys;
+    private Object[] values;
+    private int size;
 
+    public MyHashMap(){
+        keys = new Object[0];
+        values = new Object[0];
+        size = 0;
+    }
+
+    public V put(K key, V value) {
+        Object[] newKeys = new Object[size + 1];
+        Object[] newValues = new Object[size + 1];
+
+        for(int i = 0 ; i < size; i++) {
+            if(keys[i].equals(key)){
+                values[i] = value;
+                return (V) Integer.valueOf(1);
+            }
+            newKeys[i] = keys[i];
+        }
+        for(int i = 0; i < size; i++)
+            newValues[i] = values[i];
+        newKeys[size] = key;
+        newValues[size] = value;
+        size++;
+
+        keys = newKeys;
+        values = newValues;
+
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
 }
---
+++ src/test/java/com/ll/MyHashMapTest.java
@@ -0,0 +1,37 @@
+package com.ll;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class MyHashMapTest {
+    private MyHashMap<String, Integer> map;
+
+    @BeforeEach
+    void setUp() {
+        map = new MyHashMap<>();
+    }
+
+    @Test
+    void testSize() {
+        map.put("Key1", 1);
+        map.put("Key2", 2);
+        map.put("Key3", 3);
+        assertEquals(3, map.size());
+    }
+
+    @Test
+    void testPut() {
+        assertNull(map.put("Key1", 1));
+        assertEquals(1, map.size());
+    }
+
+    @Test
+    void testPutReplace() {
+        map.put("Key1", 1);
+        assertEquals(Integer.valueOf(1), map.put("Key1", 2));
+        assertEquals(1, map.size());
+    }
+}
